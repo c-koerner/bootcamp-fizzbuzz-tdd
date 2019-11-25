94d917632ada54e8eccbc67f4aaaa32116ca1cac
Christian Körner <christian.ck.koerner@googlemail.com>
2019-11-25 14:20:18 +0100

    fixes tests, added function, added main loop

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,32 @@ package org.example.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String...args) {
-    	// TODO implement me
+        // TODO implement me
+        // 1, send, ++, loop
+        int i = 1;
+        while (i < 101) { 
+            String result = fizzbuzz(i);
+            System.out.print(result);
+            if (i<100){
+                System.out.print(", ");
+            
+            }
+            i++;
+        } 
+
+
+        
     }
 
     public static String fizzbuzz(int number) {
-    	// TODO implement me
-	    return Integer.toString(15);
+        
+        if ( number % 15 == 0) {
+            return "FizzBuzz";
+        } else if ( number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else { return Integer.toString(number);
+        }
     }
 }
